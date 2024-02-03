505b356166d1fdbc0338400e58cf6a3967c6a4d0
Thiago Lopes <tiagojosecnn@outlook.com>
2024-02-03 00:56:55 -0300

    inicia página inicial.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,34 @@
 import './pages.css'
 
 export default function Home () {
-    return <h3>testando</h3>
+    return (
+        <>
+        <div className='container1'>
+            <div className='menu-lateral-home'>
+                <div className='filter'>
+                    <nav className='nav-search'>
+                        <ul>
+                            <li>logo</li>
+                            <li>Home</li>
+                            <li>search</li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className='library'>library</div>
+            </div>
+
+            <div className='home-area'>
+                <nav className='nav-bar-home'></nav>
+                <div>playlists</div>
+                <div>sleep</div>
+
+                <di>
+                    <div>das</div>
+                    <div></div>
+                </di>
+            </div>
+        </div>
+        <footer>fim</footer>
+        </>
+    );
 }
