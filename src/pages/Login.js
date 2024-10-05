97d690eece0868de67f71715f02ca891f8ec4df7
THIAGO JOSE LOPES <thiago.jose@estudante.ufjf.br>
2024-10-05 21:02:48 +0000

    corrige layout do login.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-
-import logo from '../assets/Spotify_Logo_CMYK_White.png';
 import './style/loginPage.css';
 import './style/registro.css';
+import logo from '../assets/Spotify_Primary_Logo_RGB_White.png'
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
     // State para armazenar o email, senha e mensagens de erro/sucesso
@@ -11,7 +11,7 @@ export default function Login() {
     const [senha, setSenha] = useState('');
     const [msg, setMsg] = useState('');
     const [msgErro, setMsgErro] = useState('');
-
+    const navigate = useNavigate();
 
     // Função para lidar com o processo de login
     const realizarLogin = async (event) => {
@@ -29,35 +29,31 @@ export default function Login() {
 
     return (
         <div className="page-login">
-            <div className="logo-login">
-                {/* Link para a página inicial */}
-                <Link to={'/'}><img src={logo} alt='logo'/></Link>
-            </div>
-
-            <div className='center'>
-                <div className='campo-login'>
-                    <h1>Entrar no Spotify</h1>
+            <div className='campo-login'>
+                <img src={logo} alt='logo' className='logo-login' onClick={()=> {navigate('/')}}></img>
+                <h1>Entrar no Spotify</h1>
+                <div className='login'>
                     {/* Exibe mensagem de erro */}
                     <div className='msgErro center'>{msgErro}</div>
-                    <form className='login'>
+                    <form>
                         <label>E-mail ou nome de usuário</label>
                         {/* Campo de entrada para o email */}
                         <input type='text' name='email' value={email} placeholder='E-mail ou nome de usuário' onChange={(e) => {setEmail(e.target.value); setMsg(''); setMsgErro('')}}></input>
+                    
                         {/* Exibe mensagem de erro para o email */}
                         <div className='avisoErro'>{msg}</div> 
-
                         <label>Senha</label>
                         {/* Campo de entrada para a senha */}
                         <input name='senha' value={senha}  placeholder='Senha' type='password' onChange={(e) => {setSenha(e.target.value); setMsgErro('')}}></input>
-                        
+                    
                         {/* Botão para enviar o formulário de login */}
                         <button type='submit' onClick={realizarLogin}>Entrar</button>
                     </form>
-
-                    <span><Link>Esqueceu sua senha?</Link></span>
-                    {/* Link para a página de registro */}
-                    <span>Não tem uma conta <Link to={'/registro'}>inscreva-se no Spotify </Link></span>
                 </div>
+                
+                <span><Link>Esqueceu sua senha?</Link></span>
+                {/* Link para a página de registro */}
+                <span>Não tem uma conta <Link to={'/registro'}>inscreva-se no Spotify </Link></span>
             </div>
             <div className='center footer-login'>
                 {/* Mensagem de rodapé */}
