6f86db02f36b4611de052566b972e9035a2e7fdb
THIAGO JOSE LOPES <tiagojosecnn@outlook.com>
2024-10-18 23:19:25 +0000

    altera aside da home e testa conexão com api.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import './style/home.css';
 import { useNavigate } from 'react-router-dom';
 import logo from '../assets/Spotify_Primary_Logo_RGB_White.png'
+import library from '../assets/library.png';
+import create from '../assets/plus.png';
+import wwwImage from '../assets/globe.png'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { clientCredentials } from '../utils/APIRoutes';
@@ -13,22 +16,22 @@ export default function Home() {
   useEffect(() => {
     async function fetchClientCredentials() {
       await axios.get(clientCredentials)
-      .then(response => {
-        console.log("Resposta: ", response);
-        localStorage.setItem("clientCredentials", response.data.access_token);
-      })
-      .catch(error => {
-        console.log("Erro na requisição da api: ", error);
-      })  
+        .then(response => {
+          console.log("Resposta: ", response);
+          localStorage.setItem("clientCredentials", response.data.access_token);
+        })
+        .catch(error => {
+          console.log("Erro na requisição da api: ", error);
+        })
     }
 
     //Executa a função quando o componente é montado e depois a cada 55mim
     fetchClientCredentials();
-    const interval =  setInterval(fetchClientCredentials, 1000 * 60 * 55);
+    const interval = setInterval(fetchClientCredentials, 1000 * 60 * 55);
 
     //Limpa o intervalo quando o componente for desmontado
     return () => clearInterval(interval);
-  },[])
+  }, [])
 
   const navigate = useNavigate();
   const [dataSearch, setDataSearch] = useState('');
@@ -36,28 +39,64 @@ export default function Home() {
   const search = (value) => {
     setDataSearch(value);
   };
-  
+
   return (
     <div className='container spaceBetweenColumn2 col-12'>
-      
+
       <header className='header-home col-12'>
         <div className='divImageHeader'>
-          <img src={logo} alt='logo' title='Spotify' className='logo' onClick={() => navigate('/')}/>
+          <img src={logo} alt='logo' title='Spotify' className='logo' onClick={() => navigate('/')} />
         </div>
         <div className='home-search'>
-          <Search onInputChange={search}/>
+          <Search onInputChange={search} />
         </div>
         <div className='buttonsHomeHeader'>
-            <button className='subscribe-btn' onClick={()=> navigate('/registro')}>Inscreva-se</button>
-            <button className='login-btn' onClick={()=> navigate('/login')}>Entrar</button>
-          </div>
+          <button className='subscribe-btn' onClick={() => navigate('/registro')}>Inscreva-se</button>
+          <button className='login-btn' onClick={() => navigate('/login')}>Entrar</button>
+        </div>
       </header>
 
       <div className='row-1'>
-        
+
         <section className='section_type_1'>
-          <aside>teste</aside>
-          <MainContent params={dataSearch}/>
+          <aside>
+
+            <div className='header-aside'>
+              <span><img src={library} alt='Sua biblioteca' title='Ocultar Sua Biblioteca' style={{ marginRight: '5px' }} /> Sua Biblioteca</span>
+              <div className='create'>
+                <img src={create} alt='Criar playlist ou pasta' title='Criar playlist ou pasta' style={{ width: '18px' }} />
+              </div>
+            </div>
+
+            <div className='main-aside'>
+              <article>
+                <p style={{ fontWeight: '600' }}>Crie sua primeira playlist</p>
+                <p style={{fontSize:'small'}}>É fácil vamos te ajudar.</p>
+                <button>Criar Playlist</button>
+              </article>
+
+              <article>
+                <p style={{ fontWeight: '600' }}>Que tal seguir um podcast novo?</p>
+                <p style={{fontSize:'small'}}>Avisaremos você sobre novos episódios.</p>
+                <button>Explore podcasts</button>
+              </article>
+            </div>
+
+            <div className='footer-aside'>
+              <article>
+                <pre>Legal    Segurança e Centro de privacidade</pre>
+                <pre>Política de privacidade    Cookies   Sobre anúncios</pre>
+                <pre>Acessibilidade</pre>
+              </article>
+
+              <div className='language'>
+                <img src={wwwImage} alt='idioma' style={{width: '17px'}}/>
+                <span>Português do Brasil</span>
+              </div>
+            </div>
+
+          </aside>
+          <MainContent params={dataSearch} />
         </section>
 
         {/* Rodapé */}
@@ -66,11 +105,11 @@ export default function Home() {
             <h5>Preview of Spotify</h5>
             <p>Sign up to get unlimited songs and podcasts with occasional ads. No credit card needed.</p>
           </div>
-          <button onClick={() =>{navigate(`/registro`)}}>Sign up free</button>
+          <button onClick={() => { navigate(`/registro`) }}>Sign up free</button>
         </footer>
       </div>
-      
-      
+
+
     </div>
   );
 }
--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,2 +1,3 @@
-export const host = "http://localhost:3333/";
-export const clientCredentials = `${host}api/auth/clientCredentials`;
+//export const host = "http://localhost:3333/";
+export const host = "https://special-space-guide-p4pwjxxj9qcr9g-59282.app.github.dev";
+export const clientCredentials = `${host}/api/auth/clientCredentials`;
